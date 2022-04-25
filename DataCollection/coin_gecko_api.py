9fb781ad06a613b05fc1375526c1bb7d358e094f
SzakacsAdam <szakacs.adam.98@gmai.com>
2022-04-25 18:41:48 +0200

    ADD: request handler

--- DataCollection/coin_gecko_api.py
+++ DataCollection/coin_gecko_api.py
@@ -1,4 +1,10 @@
 from requests import Session
+from json import loads
+from requests.exceptions import HTTPError
+from requests.exceptions import ConnectionError
+from requests.exceptions import Timeout
+from requests.exceptions import RequestException
+
 
 class CoinGeckoAPI:
     __API_URL_BASE: str = "https://api.coingecko.com/api/v3"
@@ -7,4 +13,19 @@ class CoinGeckoAPI:
         self.api_base_url = api_base_url
         self.session = Session()
         self.timeout: int = 60
-    
+
+    def __request(self, url) -> str:
+        try:
+            response = self.session.get(url, timeout=self.timeout)
+            response.raise_for_status()
+        except HTTPError:
+            raise
+        except ConnectionError:
+            raise
+        except Timeout:
+            raise
+        except RequestException:
+            raise
+
+        else:
+            return loads(response.content.decode('utf-8'))
